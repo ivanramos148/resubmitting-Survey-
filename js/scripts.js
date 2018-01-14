883af2ffcbfc0bff90afd1a1a3c3ef3a09cae9d8
ivanramos148248@gmail.com <ivanramos148248@gmail.com>
2018-01-13 21:03:46 -0800

    added select options

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,15 @@
 $(document).ready(function() {
+   $("form#formOne").submit(function(event) {
+     var school = $("select#school").val();
+     var years = $("select#years").val();
 
-}
+    if (school  === 'not1' && years === '1'); {
+      
+    }
+
+      $("#trail1").show();
+
+
+      event.preventDefault();
+  });
+});
