22d35d09e0af6b5db7d5634a1c14bb18f4049bf0
ivanramos148248@gmail.com <ivanramos148248@gmail.com>
2018-01-21 11:00:35 -0800

    change better for the user

--- js/scripts.js
+++ js/scripts.js
@@ -1,56 +1,27 @@
 $(document).ready(function() {
-  $("form#formOne").submit(function(event) {
-    var plan = $("select#plan").val();
-    var exprienced = $("select#exprienced").val();
-    var passion = $("select#passion").val();
-    var year = $("select#year").val();
-    var prefer = $("select#prefer").val();
-    var dif = $("select#dif").val();
+    $("form#formOne").submit(function(event) {
+    event.preventDefault()
+		$('#trail1').hide();
+		$('#trail2').hide();
+		$('#path').hide();
 
+ var plan = $("select#plan").val();
+ var exprienced = $("select#exprienced").val();
+ var passion = $("select#passion").val();
+ var year = $("select#year").val();
+ var prefer = $("select#prefer").val();
+ var dif = $("select#dif").val();
 
-    if (plan === 'yes')
-      {
-    };
-    if (exprienced === 'yes1')
-      {
-    };
-    if (passion === 'yes2')
-      {
-    };
-    if (year === 'yes3')
-      {
-    };
-    if (prefer === 'college')
-      {
-      $('#trail1').show();
-    event.preventDefault();
-  }else{
-    if (plan === 'no')
-      {
-    };
-    if (exprienced === 'no1')
-      {
-    };
-    if(passion === 'no2')
-      {
-    };
-    if (year === 'no3')
-      {
-    };
-    if (prefer === 'boot')
-      {
-      $('#trail2').show();
-      event.preventDefault();
-    }else{
-
-      if (dif === 'hard')
-        {
-      $('#path').show(); {
-    };
-    event.preventDefault();
-}
-
-}
-}
-});
+ if ((dif === 'easy') && (plan === 'yes') && (exprienced === 'yes1') || (passion === 'yes2') || (year === 'yes3') || (prefer === 'college')) {
+$('#trail1').show();
+} else {
+ if ((dif === 'hard') && (plan === 'no') && (exprienced === 'no1') || (passion === 'no2') || (year === 'no3') || (prefer === 'boot')) {
+  $('#trail2').show();
+} else  {
+  if ((dif === 'na') && (plan === 'na') && (exprienced === 'na') || (passion === 'na') || (year === 'na') || (prefer === 'na')) {
+      $('#path').show(); {};
+         }
+       }
+     }
+   });
 });
